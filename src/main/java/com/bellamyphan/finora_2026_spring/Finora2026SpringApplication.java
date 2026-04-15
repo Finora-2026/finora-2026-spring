225f75c5206be18f7d3cb873d7d3cebbf720fc56
bellamyphan <bellamyphan@icloud.com>
2026-04-15 15:47:37 -0500

    Initial commit

---
+++ src/main/java/com/bellamyphan/finora_2026_spring/Finora2026SpringApplication.java
@@ -0,0 +1,13 @@
+package com.bellamyphan.finora_2026_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Finora2026SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Finora2026SpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bellamyphan/finora_2026_spring/Finora2026SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bellamyphan.finora_2026_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Finora2026SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
