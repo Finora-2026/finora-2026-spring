7a2ff1249faa86a56dcd151e1633e7f0555004c3
Bellamy Phan <bellamyphan@icloud.com>
2026-04-20 01:39:17 -0500

    FI-146: merge codes from main branch

--- src/main/java/com/bellamyphan/finora_2026_spring/mongodb/controller/MovieController.java
+++ src/main/java/com/bellamyphan/finora_2026_spring/mongodb/controller/MovieController.java
@@ -5,10 +5,6 @@ import com.bellamyphan.finora_2026_spring.mongodb.service.MovieService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
