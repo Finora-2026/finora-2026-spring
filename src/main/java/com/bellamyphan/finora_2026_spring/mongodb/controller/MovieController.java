47647b419df81d24ac8d9b4a8ff37a369eeb3fc9
Bellamy Phan <55559722+bellamyphan@users.noreply.github.com>
2026-04-20 01:41:21 -0500

    Fi 146 return list of movie and can filter movie (#9)
    * FI-146: add repository, controller and service to return all movies or filter by movie imdb id
    
    * FI-146: add repository, controller and service to return all movies or filter by movie imdb id
    
    * FI-146: setting CrossOrigin policies
    
    * FI-146: merge codes from main branch

--- src/main/java/com/bellamyphan/finora_2026_spring/mongodb/controller/MovieController.java
+++ src/main/java/com/bellamyphan/finora_2026_spring/mongodb/controller/MovieController.java
@@ -5,10 +5,7 @@ import com.bellamyphan.finora_2026_spring.mongodb.service.MovieService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,6 +13,10 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/movies")
 @AllArgsConstructor
+@CrossOrigin(origins = {
+        "http://localhost:5173",
+        "https://bellamyphan.com"
+})
 public class MovieController {
 
     private final MovieService movieService;
