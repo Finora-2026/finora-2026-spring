f1060d48d71471643c0089e206c50151eb33019e
bellamyphan <bellamyphan@icloud.com>
2026-05-05 17:47:33 -0500

    FI-168: create UserController to allow creating new users

---
+++ src/main/java/com/bellamyphan/finora_2026_spring/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.bellamyphan.finora_2026_spring.controller;
+
+import com.bellamyphan.finora_2026_spring.dto.UserCreateRequestDto;
+import com.bellamyphan.finora_2026_spring.dto.UserCreateResponseDto;
+import com.bellamyphan.finora_2026_spring.entity.User;
+import com.bellamyphan.finora_2026_spring.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    // Create a new user (public API)
+    @PostMapping
+    public ResponseEntity<?> createUser(@Valid @RequestBody UserCreateRequestDto userRequest) {
+
+        User savedUser = userService.createUser(userRequest);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(UserCreateResponseDto.fromEntity(savedUser));
+    }
+}
---
+++ src/main/java/com/bellamyphan/finora_2026_spring/dto/UserCreateRequestDto.java
@@ -0,0 +1,22 @@
+package com.bellamyphan.finora_2026_spring.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class UserCreateRequestDto {
+
+    private String name;
+
+    @Email
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+
+    private String role;
+}
