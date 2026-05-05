f28adfb1fd0dae8fbdc3fd20bab5d6ba2363b69a
bellamyphan <bellamyphan@icloud.com>
2026-05-05 17:45:58 -0500

    FI-168: create role and user entity/repository

---
+++ src/main/java/com/bellamyphan/finora_2026_spring/entity/Role.java
@@ -0,0 +1,28 @@
+package com.bellamyphan.finora_2026_spring.entity;
+
+import com.bellamyphan.finora_2026_spring.constant.RoleEnum;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "roles")
+@Data // Lombok will generate getters, setters, toString, equals, and hashCode methods
+@NoArgsConstructor
+public class Role {
+
+    @Id
+    @Column(name = "id", nullable = false, length = 10)
+    private String id; // NanoID 10-char
+
+    @Enumerated(EnumType.STRING) // Store enum as string in DB
+    @Column(name = "name", nullable = false, unique = true, length = 50)
+    @NotNull(message = "Role is required")
+    private RoleEnum name;
+
+    // Constructor with only enum (id can be generated separately)
+    public Role(RoleEnum name) {
+        this.name = name;
+    }
+}
