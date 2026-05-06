0cade200517befd7d03255eb90b533eeda79bd21
bellamyphan <bellamyphan@icloud.com>
2026-05-06 17:11:07 -0500

    FI-176: remove CORS policy at controller level

--- src/main/java/com/bellamyphan/finora_2026_spring/controller/UserController.java
+++ src/main/java/com/bellamyphan/finora_2026_spring/controller/UserController.java
@@ -13,10 +13,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
-@CrossOrigin(origins = {
-        "http://localhost:5173",
-        "https://bellamyphan.com"
-})
 @RequiredArgsConstructor
 public class UserController {
 
