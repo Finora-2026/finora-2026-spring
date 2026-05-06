8db6df1afdd131d0e6a09ec29b6348d4bc907fca
bellamyphan <bellamyphan@icloud.com>
2026-05-06 15:40:34 -0500

    FI-175: make UserController to be accessible CORS policy

--- src/main/java/com/bellamyphan/finora_2026_spring/controller/UserController.java
+++ src/main/java/com/bellamyphan/finora_2026_spring/controller/UserController.java
@@ -9,13 +9,14 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
+@CrossOrigin(origins = {
+        "http://localhost:5173",
+        "https://bellamyphan.com"
+})
 @RequiredArgsConstructor
 public class UserController {
 
