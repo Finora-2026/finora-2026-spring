5bf2059ef89d12c0cb3e49458169699c5eb087b0
Bellamy Phan <bellamyphan@icloud.com>
2026-05-12 23:19:45 -0500

    FI-260: generate brand on start

---
+++ src/main/java/com/bellamyphan/finora_2026_spring/service/BrandService.java
@@ -0,0 +1,38 @@
+package com.bellamyphan.finora_2026_spring.service;
+
+import com.bellamyphan.finora_2026_spring.dto.BrandCreateRequestDto;
+import com.bellamyphan.finora_2026_spring.dto.BrandCreateResponseDto;
+import com.bellamyphan.finora_2026_spring.entity.Brand;
+import com.bellamyphan.finora_2026_spring.repository.BrandRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class BrandService {
+
+    private final NanoIdService nanoIdService;
+
+    private final BrandRepository brandRepository;
+
+    @Transactional
+    public BrandCreateResponseDto createBrand(BrandCreateRequestDto request) {
+        // Normalize name and URL
+        String normalizedName = request.getName().trim();
+        String urlNormalized = request.getUrl() != null ? request.getUrl().trim().toLowerCase() : null;
+
+        // Check for name or url duplication
+        if (brandRepository.existsByNameIgnoreCase(normalizedName)) {
+            throw new IllegalArgumentException("Brand name already exists: '" + normalizedName + "'");
+        }
+        if (urlNormalized != null && !urlNormalized.isBlank() && brandRepository.existsByUrlIgnoreCase(urlNormalized)) {
+            throw new IllegalArgumentException("Brand URL already exists: '" + urlNormalized + "'");
+        }
+
+        // Create brand and save to DB.
+        Brand brand = new Brand(normalizedName, urlNormalized);
+        brand.setId(nanoIdService.generateUniqueId(brandRepository));
+        return BrandCreateResponseDto.fromEntity(brandRepository.save(brand));
+    }
+}
