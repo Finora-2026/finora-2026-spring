1adfa0abd18c49efe3423a3039f9f89d69efc3b6
Bellamy Phan <bellamyphan@icloud.com>
2026-05-12 23:08:17 -0500

    FI-258: generate transaction type on start

--- src/main/java/com/bellamyphan/finora_2026_spring/repository/AccountRepository.java
+++ src/main/java/com/bellamyphan/finora_2026_spring/repository/AccountRepository.java
@@ -3,9 +3,11 @@ package com.bellamyphan.finora_2026_spring.repository;
 import com.bellamyphan.finora_2026_spring.entity.Account;
 import com.bellamyphan.finora_2026_spring.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface AccountRepository extends JpaRepository<Account, String> {
 
     List<Account> findByUser(User user);
