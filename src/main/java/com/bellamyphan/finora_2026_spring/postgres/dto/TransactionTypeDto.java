eaca3a38f21914c0e97e08f418eec60c63e677d6
Bellamy Phan <bellamyphan@icloud.com>
2026-05-14 17:28:40 -0500

    FI-280: Get all transactionType using controller

---
+++ src/main/java/com/bellamyphan/finora_2026_spring/postgres/dto/TransactionTypeDto.java
@@ -0,0 +1,11 @@
+package com.bellamyphan.finora_2026_spring.postgres.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class TransactionTypeDto {
+    private String id;
+    private String name;
+}
