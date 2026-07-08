58f4520f7bb324eb5373b780ea6cf3ace782f6ea
bellamyphan <bellamyphan@icloud.com>
2026-07-08 16:55:24 -0500

    FI-453: BE system can fetch last posted and current pending reports

--- src/main/java/com/bellamyphan/finora_2026_spring/postgres/repository/ReportRepository.java
+++ src/main/java/com/bellamyphan/finora_2026_spring/postgres/repository/ReportRepository.java
@@ -1,9 +1,15 @@
 package com.bellamyphan.finora_2026_spring.postgres.repository;
 
 import com.bellamyphan.finora_2026_spring.postgres.entity.Report;
+import com.bellamyphan.finora_2026_spring.postgres.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ReportRepository extends JpaRepository<Report, String> {
+
+    Optional<Report> findTopByUserAndIsPostedTrueOrderByMonthDesc(User user);
+    Optional<Report> findTopByUserAndIsPostedFalseOrderByMonthDesc(User user);
 }
